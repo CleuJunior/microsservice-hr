c539776fb98d1237b2cdb7075e933668d6799060
Cleonildo Jr <cleu.junior@gmail.com>
2022-07-12 22:39:54 -0300

    Fixing Cloud Issues

--- hr-payroll/src/main/java/com/ms/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ms/hrpayroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package com.ms.hrpayroll.services;
 
+import com.ms.hrpayroll.feingclients.WorkerFeignClient;
 import com.ms.hrpayroll.model.Payment;
 import com.ms.hrpayroll.model.Worker;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class PaymentService {
+    private final WorkerFeignClient workerFeignClient;
 
-    @Value("${hr-worker.host}")
-    private String host;
-    private final RestTemplate restTemplate;
-
-    public PaymentService(final RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public PaymentService(final WorkerFeignClient workerFeignClient) {
+        this.workerFeignClient = workerFeignClient;
     }
     public Payment getPayment(UUID workerId, int days) {
-        Map<String, UUID> uriVariables = new HashMap<>();
-
-        uriVariables.put("id", workerId);
-        Worker worker = this.restTemplate.getForObject(this.host + "/api/workers/{id}", Worker.class, uriVariables);
+        Worker worker = this.workerFeignClient.findWorkByID(workerId).getBody();
 
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
